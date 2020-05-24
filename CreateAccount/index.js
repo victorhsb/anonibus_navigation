c9a40d5c39e90a08a7fa7b805ef0ce29aa0e148b
Antonio Podgorski <podgorski1895@gmail.com>
2020-05-24 16:30:36 -0300

    chore:added textinputs in anonibus navigation

--- CreateAccount/index.js
+++ CreateAccount/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import { } from './styles';
 
 import { AuthContext } from '../context';
@@ -11,16 +11,28 @@ export default CreateAccount = () => {
   const handleSignUp = () => {
     firebase
       .auth()
-      .createUserWithEmailAndPassword("antonio@podgorski.com.br", "123123")
+      .createUserWithEmailAndPassword(textEmail, textPassword)
       .then(() => signUp())
       .catch(error => alert(error))
   }
 
   const { signUp } = React.useContext(AuthContext)
+  const [textEmail, setTextEmail] = React.useState('')
+  const [textPassword, setTextPassword] = React.useState('')
 
   return (
     <View style={styles.container}>
-      <Text>CREATEACCOUNT</Text>
+      <View style={styles.footer}>
+        <TextInput
+          style={styles.input_mensagem}
+          onChangeText={text => setTextEmail(text)}
+          value={textEmail} />
+
+        <TextInput
+          style={styles.input_mensagem}
+          onChangeText={text => setTextPassword(text.toLowerCase())}
+          value={textPassword} secureTextEntry={true} />
+      </View>
       <Button title="Criar Conta" onPress={() => handleSignUp()} />
     </View>
   )
@@ -37,6 +49,20 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     marginVertical: 10,
     borderRadius: 5
+  },
+  input_mensagem: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    flex: 1,
+    borderRadius: 3,
+    margin: 10,
+    marginTop: 0,
+    padding: 4
+  },
+  footer: {
+    flexDirection: 'column',
+    width: '100%',
+    height: 100
   }
 });
 
--- SignIn/index.js
+++ SignIn/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 
 import { AuthContext } from '../context';
 
@@ -10,12 +10,16 @@ import firebase from 'firebase';
 
 export default SignIn = ({ navigation }) => {
 
+
+
   const { signIn } = React.useContext(AuthContext)
+  const [textEmail, setTextEmail] = React.useState('')
+  const [textPassword, setTextPassword] = React.useState('')
 
   const handleSignIn = () => {
     firebase
       .auth()
-      .signInWithEmailAndPassword("antonio@podgorski.com.br", "123123")
+      .signInWithEmailAndPassword(textEmail, textPassword)
       .then(() => signIn())
       .catch(error => alert(error))
 
@@ -23,7 +27,19 @@ export default SignIn = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text>SIGNIN</Text>
+      <View style={styles.footer}>
+        <TextInput
+          style={styles.input_mensagem}
+          onChangeText={text => setTextEmail(text)}
+          value={textEmail} />
+
+        <TextInput
+          style={styles.input_mensagem}
+          onChangeText={text => setTextPassword(text.toLowerCase())}
+          value={textPassword} secureTextEntry={true} />
+      </View>
+
+
       <Button title="Acessar" onPress={() => handleSignIn()} />
       <Button title="Criar Conta" onPress={() => navigation.push("CreateAccount")} />
     </View>
@@ -41,6 +57,20 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     marginVertical: 10,
     borderRadius: 5
+  },
+  input_mensagem: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    flex: 1,
+    borderRadius: 3,
+    margin: 10,
+    marginTop: 0,
+    padding: 4
+  },
+  footer: {
+    flexDirection: 'column',
+    width: '100%',
+    height: 100
   }
 });
 
